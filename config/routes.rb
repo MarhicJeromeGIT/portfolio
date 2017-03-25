f0055f9ea7cd5d54b65d86294e41c29220d9f26d
jerome <marhic.jerome@gmail.com>
2017-03-25 22:11:26 +0000

    Translation and contact form in progress

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  scope "(:locale)", :locale => /en|fr|ja/ do
+    root 'static_pages#index'
 
-  root 'static_pages#index'
-
-  get 'credits', to: 'static_pages#credits'
-  get 'resume', to: 'resume#resume', defaults: {format: 'html'}
-  get 'stocks_jpg', to: 'static_pages#stocks_jpg', defaults: {format: 'jpg'}
-
+    get 'credits', to: 'static_pages#credits'
+    get 'resume', to: 'resume#resume', defaults: {format: 'html'}
+    get 'stocks_jpg', to: 'static_pages#stocks_jpg', defaults: {format: 'jpg'}
+  end
 end
